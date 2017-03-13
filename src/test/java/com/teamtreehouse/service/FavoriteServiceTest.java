bb2549b12858d6a57f8da74003524a177773acf7
konanov <konanov@imp-m.ru>
2017-03-13 16:27:45 +0300

    testing service

--- src/test/java/com/teamtreehouse/service/FavoriteServiceTest.java
+++ src/test/java/com/teamtreehouse/service/FavoriteServiceTest.java
@@ -11,7 +11,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
@@ -27,29 +27,25 @@ public class FavoriteServiceTest {
     private FavoriteService service = new FavoriteServiceImpl();
 
     @Test
-    public void findAll_ShouldReturnTwo() throws Exception {
-        List<Favorite> faves = Arrays.asList(
-            new Favorite(),
-            new Favorite()
-        );
-
+    public void findAll_ShouldReturnTwo() {
+        List<Favorite> faves = Arrays.asList(new Favorite(), new Favorite());
         when(dao.findAll()).thenReturn(faves);
-
-        assertEquals("findAll should return two favorites",2,service.findAll().size());
+        assertEquals("findAll should return two favorites", 2, service.findAll().size());
         verify(dao).findAll();
     }
 
     @Test
-    public void findById_ShouldReturnOne() throws Exception {
+    public void findById_ShouldReturnOne() {
         when(dao.findOne(1L)).thenReturn(new Favorite());
-        assertThat(service.findById(1L),instanceOf(Favorite.class));
+        assertThat(service.findById(1L), instanceOf(Favorite.class));
         verify(dao).findOne(1L);
     }
 
     @Test(expected = FavoriteNotFoundException.class)
-    public void findById_ShouldThrowFavoriteNotFoundException() throws Exception {
+    public void findById_ShouldThrowFavoriteNotFoundException() {
         when(dao.findOne(1L)).thenReturn(null);
         service.findById(1L);
-        verify(dao).findOne(1L);
+        verify(service).findById(1L);
     }
+
 }
--- src/test/java/com/teamtreehouse/service/WeatherServiceTest.java
+++ src/test/java/com/teamtreehouse/service/WeatherServiceTest.java
@@ -26,44 +26,21 @@ import static org.hamcrest.Matchers.hasSize;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class WeatherServiceTest {
-    @Autowired
-    private WeatherService service;
 
-    private Location loc;
+    @Autowired
+    private WeatherService weatherService;
+
+    private Location location;
     private Weather weather;
 
     private static final double ERROR_GEO = 0.0000001;
     private static final double ERROR_TIME = 5000;
 
-    @Before
-    public void setup() {
-        loc = new Location(41.9403795,-87.65318049999999);
-        weather = service.findByLocation(loc);
-    }
-
-    @Test
-    public void findByLocation_ShouldReturnSameCoords() throws Exception {
-        assertThat(weather.getLatitude(),closeTo(loc.getLatitude(),ERROR_GEO));
-        assertThat(weather.getLongitude(),closeTo(loc.getLongitude(),ERROR_GEO));
-    }
-
-    @Test
-    public void findByLocation_ShouldReturn8DaysForecastData() throws Exception {
-        assertThat(weather.getDaily().getData(),hasSize(8));
-    }
-
-    @Test
-    public void findByLocation_ShouldReturnCurrentConditions() throws Exception {
-        Instant now = Instant.now();
-        double duration = Duration.between(now,weather.getCurrently().getTime()).toMillis();
-        assertThat(duration,closeTo(0,ERROR_TIME));
-    }
-
     @Configuration
     @PropertySource("api.properties")
     public static class TestConfig {
         @Autowired
-        private Environment env;
+        private Environment environment;
 
         @Bean
         public RestTemplate restTemplate() {
@@ -72,12 +49,35 @@ public class WeatherServiceTest {
 
         @Bean
         public WeatherService weatherService() {
-            WeatherService service = new WeatherServiceImpl(
-                env.getProperty("weather.api.name"),
-                env.getProperty("weather.api.key"),
-                env.getProperty("weather.api.host")
+            return new WeatherServiceImpl(
+                    environment.getProperty("weather.api.name"),
+                    environment.getProperty("weather.api.key"),
+                    environment.getProperty("weather.api.host")
             );
-            return service;
         }
+    }
+
+    @Before
+    public void setup() {
+        location = new Location(41.9403795, -87.65318049999999);
+        weather = weatherService.findByLocation(location);
+    }
+
+    @Test
+    public void findByLocation_ShouldReturnSameCoords() throws Exception {
+        assertThat(weather.getLatitude(), closeTo(location.getLatitude(), ERROR_GEO));
+        assertThat(weather.getLongitude(), closeTo(location.getLongitude(), ERROR_GEO));
+    }
+
+    @Test
+    public void findByLocation_ShouldReturn8DaysForecastData() throws Exception {
+        assertThat(weather.getDaily().getData(), hasSize(8));
+    }
+
+    @Test
+    public void findByLocation_ShouldReturnCurrentConditions() throws Exception {
+        Instant now = Instant.now();
+        double duration = Duration.between(now, weather.getCurrently().getTime()).toMillis();
+        assertThat(duration, closeTo(0, ERROR_TIME));
     }
 }
